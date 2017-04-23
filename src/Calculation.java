4318d84ba73359ddbd9d81c7bdba20b8cec270f6
Raja987 <sgraja32@gmail.com>
2017-04-23 21:11:42 +0530

    Modified sysout

--- src/Calculation.java
+++ src/Calculation.java
@@ -2,6 +2,6 @@
 public class Calculation {
 	public static void main(String[] args) {
 		System.out.println("My New Project");
-		System.out.println("Only visible in new branch");
+		System.out.println("Only visible in new branch In master");
 	}
 }
